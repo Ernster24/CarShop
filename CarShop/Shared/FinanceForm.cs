41fdb641544040b2758ec64f5a0708b459d695ed
Ernie <ernieg2409@gmail.com>
2023-03-01 10:02:56 +0000

    Started to create finance form

---
+++ CarShop/Shared/FinanceForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.Shared.Forms
+{
+    public class FinanceForm
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
