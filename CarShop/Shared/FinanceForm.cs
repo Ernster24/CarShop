31957285557ecadfbd84ce99ecf79d8bded1d2de
Ernie <ernieg2409@gmail.com>
2023-03-03 21:14:30 +0000

    Further additions

--- CarShop/Shared/FinanceForm.cs
+++ CarShop/Shared/FinanceForm.cs
@@ -12,5 +12,12 @@ namespace CarShop.Shared.Forms
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Email { get; set; }
+        public string? HouseNo { get; set; }
+        public string? Street { get; set; }
+        public string? City { get; set; }
+        public string? Region { get; set; } 
+        public string? PostalCode { get; set; }
+        public string? Country { get; set; }
+        public string? Phone { get; set; }
     }
 }
