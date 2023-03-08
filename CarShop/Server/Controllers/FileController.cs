40f2862a446a48d4b58628ac8a3f8081b6923e88
Ernie <ernieg2409@gmail.com>
2023-03-08 22:39:04 +0000

    Completed file upload for finance page

--- CarShop/Server/Controllers/FileController.cs
+++ CarShop/Server/Controllers/FileController.cs
@@ -25,12 +25,12 @@ namespace CarShop.Server.Controllers
             foreach(var file in files)
             {
                 var uploadResult = new UploadResult();
-                string trustedFileNameForStorage;
-                var untrustedFileName = file.FileName;
+                string trustedFileNameForStorage;   // Random string file name
+                var untrustedFileName = file.FileName;  // Actual file name of uploaded file
                 uploadResult.FileName = untrustedFileName;
                 var trustedFileNameForDisplay = WebUtility.HtmlEncode(untrustedFileName);
 
-                trustedFileNameForStorage = Path.GetRandomFileName();
+                trustedFileNameForStorage = Path.GetRandomFileName();   // Changes file name to a random string to prevent files being overwritten
                 var path = Path.Combine(_env.ContentRootPath, "Uploads", trustedFileNameForStorage);
 
                 await using FileStream fs = new(path, FileMode.Create);
